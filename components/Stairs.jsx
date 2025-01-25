76d27f73aaa4ad1ebb05eea9ceb919c7338a3b9d
muhammadsyam <muhammadsyam95@gmail.com>
2025-01-25 21:28:28 +0700

    hilangkan tanda petik

--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -24,7 +24,7 @@ const Stairs = () => {
     <>
       {/* render 6 motion divs, each representing a step of the stairs.
   Each div will have the same animation defined by the stairsAnimation object.
-  The delay for each div is calculated sinamically based on it's reversed index,
+  The delay for each div is calculated sinamically based on its reversed index,
   creating a staggered effect with decreasing delay for each subsequent step.
   */}
       {[...Array(6)].map((_, index) => {
